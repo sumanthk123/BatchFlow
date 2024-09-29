751811c6a44ae232f08566e797fe268eb3dfbd12
sd74 <sd74@illinois.edu>
2024-09-28 19:45:16 -0500

    Made Site consistent for Lightmode users

--- app/Header/page.js
+++ app/Header/page.js
@@ -1,17 +1,16 @@
-import Image from 'next/image'; // Import Next.js Image component
+import Image from 'next/image';
 import logo from '../logo-no-background.png'; 
-import '../globals.css'; // Import global styles
+import './header.css';
 
 const Header = () => {
     return (
-        <header className="relative w-full h-20 overflow-hidden bg-gray">
-            <h1 className="sr-only">BatchFlow</h1> {/* Screen reader text for accessibility */}
-            <div className="absolute top-4 left-0 right-0 bottom-0 flex items-center justify-center">
+        <header className="header-container">
+            <div className="header-logo">
                 <Image 
                     src={logo}
                     alt="BatchFlow Logo"
-                    fill // Use layout fill to cover the container
-                    className="object-contain" // Maintain aspect ratio
+                    fill
+                    className="object-contain" 
                 />
             </div>
         </header>
@@ -19,3 +18,4 @@ const Header = () => {
 };
 
 export default Header;
+

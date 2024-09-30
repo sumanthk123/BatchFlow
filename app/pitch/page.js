0b1bdb6a28d0ce347bb0892a9e545fa76fb24a9d
Sumanth Kalluru <kallurusumanth@gmail.com>
2024-09-30 14:41:53 -0500

    email + changed text

--- app/pitch/page.js
+++ app/pitch/page.js
@@ -29,33 +29,33 @@ export default function Pitch() {
             Our startup aims to create a centralized command center and version control system specifically designed for Large Language Models (LLMs). This platform will function as a GitHub-style repository for LLMs, allowing small companies and software developers to store, evaluate, and monitor their LLMs across multiple versions.
             <br />
             <br />
+            Our essential components consist of:
+            <ul className="pitch-tldr-list">
+              <li>
+                Connecting all LLM Workflow data sources (model files, datasets, prompt injections, etc.) to BatchFlow to create a source of truth for the black box of LLMs. No coding required.
+              </li>
+              <li>
+                Creating prompts personalized to datasets allowing for easy evaluation on small, in-house LLMs.
+              </li>
+              <li>
+                Integrating a version control system to easily access model analytics and compare effectiveness of different prompting techniques.
+              </li>
+            </ul>
+            <br />
             With the increasing complexity and prevalence of LLMs, the need for efficient model management and evaluation tools has become crucial. 
             Our solution will also provide an intuitive dashboard that displays essential metrics, such as accuracy, model performance, and version history, 
             allowing users to make data-driven decisions about their models.
           </div>
 
-          <div className="pitch-section-header">
-            TL;DR
-          </div> 
-          <div className="pitch-section-text">
-            <ul className="pitch-tldr-list">
-              <li>
-                Connect all LLM Workflow data sources (model files, datasets, prompt injections, etc.) to BatchFlow to create a source of truth for the black box of LLMs. No coding required.
-              </li>
-              <li>
-                BatchFlow dynamically creates prompts personalized to datasets allowing for easy evaluation on small, in-house LLMs.
-              </li>
-              <li>
-                BatchFlow displays a version control system to easily access model analytics and compare effectiveness of different prompting techniques.
-              </li>
-            </ul>
-          </div>
 
           <div className="pitch-section-header">
             Ask
           </div>
           <div className="pitch-section-text">
             We are currently seeking mentors to provide guidance and are looking for AI and database professionals for additional support.
+            <br />
+            <br />
+            <a href= "mailto: batchfloworg@gmail.com"> <u>batchfloworg@gmail.com</u> </a>
           </div>
 
         </div>
